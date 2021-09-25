b96447771f71116d17046e0b85d88d5d2ba489cf
msolorio <msolorio.github@gmail.com>
2021-09-25 10:18:01 -0700

    Adds demo code for functions and conditionals

---
+++ functions-conditionals.rkt
@@ -0,0 +1,34 @@
+(require 2htdp/image)
+
+;; stepper-starter.rkt
+
+;(triangle 40 "solid" "green")
+
+;(> 3 2)
+
+(define result (/ 5 3))
+
+(round result)
+
+
+;(+ (* 3 2) 1)
+
+;; Write a function that returns the max dimensions of a rectangle
+
+;(define (get_longest_side img)
+;  (if (> (image-width img) (image-height img))
+;         (image-width img)
+;         (image-height img)
+;  )
+;)
+
+;(get_longest_side (rectangle 30 40 "solid" "blue"))
+
+
+;(define (max-dim img)
+;  (if (> (image-width img) (image-height img))
+;      (image-width img)
+;      (image-height img)))
+
+
+;(max-dim (rectangle 10 20 "solid" "blue"))
